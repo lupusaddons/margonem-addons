ab89294983169594bc2a2f842355742fb1083bb5
lupusaddons <maciej.bialoecki11@gmail.com>
2025-08-15 15:40:31 +0200

    redroom.js

---
+++ redroom.js
@@ -0,0 +1,226 @@
+// Członkowie klanu Redroom - 109 graczy
+const guild_Lupus_149 = {
+    name: "Redroom",
+    members: [
+        "Kiniocs",
+        "Smalchini Toffichini",
+        "Ketirezov Komornikov",
+        "Skonaldo",
+        "number one yapper",
+        "kajmi",
+        "Bumson",
+        "krul podziemia",
+        "pawel z kiniem",
+        "Kaculek Nekromanta",
+        "kochac disa",
+        "Jandol",
+        "Flopp",
+        "Fizyczny Gorth",
+        "Hisagi",
+        "Zabawka Hako",
+        "Skopcona Edytka",
+        "El Trebron",
+        "Encara Messi I",
+        "amistad",
+        "Tiqson",
+        "Czarodziej Bartek",
+        "Rosaline",
+        "Twardomir",
+        "Kapusta Topowa",
+        "osobisty ochroniasz",
+        "Wolnir na Topke",
+        "Marshallowy",
+        "Mroczny Bandziorek",
+        "Efes",
+        "Varreziak",
+        "Blight",
+        "Stary Syberyjczyk",
+        "Isir",
+        "Vxyr",
+        "Gawriił Harkin",
+        "Rasmey",
+        "Browuś",
+        "Asetorek",
+        "unia prosze",
+        "Mac",
+        "zwei sieben",
+        "Al-Fedee",
+        "Skibidi Sigma Rizzler",
+        "Marcin Sigma",
+        "sigma ukraine boy",
+        "Bwakale",
+        "Exclusi",
+        "Pokaż Co Masz",
+        "Uysol",
+        "Habibi I",
+        "Kolekcjoner Ekipa",
+        "Nie Pokazujcie Mu",
+        "Marit Bjoergen",
+        "Żaba w kotle",
+        "jeden z karkuwek",
+        "Hellbomber",
+        "Treebron",
+        "Kaculek",
+        "Terrorista Keti",
+        "Maguowy Zadymiarz",
+        "Vajsensraysen",
+        "Billadenek Kox",
+        "Po Prostu Wolnir",
+        "Młodyy Al Capone",
+        "Og Farmer",
+        "Isirek",
+        "żółw-",
+        "Scozzari",
+        "knur opasy",
+        "The Shax",
+        "Slowpoke Rodriguez",
+        "Gewehr",
+        "Odmów Mi Synu",
+        "Eovacius",
+        "Sokano",
+        "Dynowiak",
+        "Yankosek",
+        "Sypie się papier",
+        "Broshey",
+        "Frax",
+        "Razta",
+        "Hastosław",
+        "Turutek",
+        "Kacpervip",
+        "Brachol",
+        "holiday pawulon",
+        "Andreus",
+        "Brr-Brr-Patapim",
+        "Frigo-Camelo",
+        "Kolczar",
+        "Scigussek",
+        "Spalacz tkanki",
+        "lupus terrorist",
+        "czołg-",
+        "Keef Papaj",
+        "Provokatiąun",
+        "Uber comfort",
+        "Rozrywkowy menix",
+        "Varrezinho",
+        "kinio z pawlem",
+        "Psotnik Łobuziak",
+        "fisherprice",
+        "Bartek Na Teze",
+        "Tabluga",
+        "Sokano na teze",
+        "San Andreas",
+        "hot ciżemka",
+        "Akwarelka"
+    ]
+};
+
+// Mapowanie gracz -> klan dla Redroom
+playerGuilds["kiniocs"] = "Redroom";
+playerGuilds["smalchini toffichini"] = "Redroom";
+playerGuilds["ketirezov komornikov"] = "Redroom";
+playerGuilds["skonaldo"] = "Redroom";
+playerGuilds["number one yapper"] = "Redroom";
+playerGuilds["kajmi"] = "Redroom";
+playerGuilds["bumson"] = "Redroom";
+playerGuilds["krul podziemia"] = "Redroom";
+playerGuilds["pawel z kiniem"] = "Redroom";
+playerGuilds["kaculek nekromanta"] = "Redroom";
+playerGuilds["kochac disa"] = "Redroom";
+playerGuilds["jandol"] = "Redroom";
+playerGuilds["flopp"] = "Redroom";
+playerGuilds["fizyczny gorth"] = "Redroom";
+playerGuilds["hisagi"] = "Redroom";
+playerGuilds["zabawka hako"] = "Redroom";
+playerGuilds["skopcona edytka"] = "Redroom";
+playerGuilds["el trebron"] = "Redroom";
+playerGuilds["encara messi i"] = "Redroom";
+playerGuilds["amistad"] = "Redroom";
+playerGuilds["tiqson"] = "Redroom";
+playerGuilds["czarodziej bartek"] = "Redroom";
+playerGuilds["rosaline"] = "Redroom";
+playerGuilds["twardomir"] = "Redroom";
+playerGuilds["kapusta topowa"] = "Redroom";
+playerGuilds["osobisty ochroniasz"] = "Redroom";
+playerGuilds["wolnir na topke"] = "Redroom";
+playerGuilds["marshallowy"] = "Redroom";
+playerGuilds["mroczny bandziorek"] = "Redroom";
+playerGuilds["efes"] = "Redroom";
+playerGuilds["varreziak"] = "Redroom";
+playerGuilds["blight"] = "Redroom";
+playerGuilds["stary syberyjczyk"] = "Redroom";
+playerGuilds["isir"] = "Redroom";
+playerGuilds["vxyr"] = "Redroom";
+playerGuilds["gawriił harkin"] = "Redroom";
+playerGuilds["rasmey"] = "Redroom";
+playerGuilds["browuś"] = "Redroom";
+playerGuilds["asetorek"] = "Redroom";
+playerGuilds["unia prosze"] = "Redroom";
+playerGuilds["mac"] = "Redroom";
+playerGuilds["zwei sieben"] = "Redroom";
+playerGuilds["al-fedee"] = "Redroom";
+playerGuilds["skibidi sigma rizzler"] = "Redroom";
+playerGuilds["marcin sigma"] = "Redroom";
+playerGuilds["sigma ukraine boy"] = "Redroom";
+playerGuilds["bwakale"] = "Redroom";
+playerGuilds["exclusi"] = "Redroom";
+playerGuilds["pokaż co masz"] = "Redroom";
+playerGuilds["uysol"] = "Redroom";
+playerGuilds["habibi i"] = "Redroom";
+playerGuilds["kolekcjoner ekipa"] = "Redroom";
+playerGuilds["nie pokazujcie mu"] = "Redroom";
+playerGuilds["marit bjoergen"] = "Redroom";
+playerGuilds["żaba w kotle"] = "Redroom";
+playerGuilds["jeden z karkuwek"] = "Redroom";
+playerGuilds["hellbomber"] = "Redroom";
+playerGuilds["treebron"] = "Redroom";
+playerGuilds["kaculek"] = "Redroom";
+playerGuilds["terrorista keti"] = "Redroom";
+playerGuilds["maguowy zadymiarz"] = "Redroom";
+playerGuilds["vajsensraysen"] = "Redroom";
+playerGuilds["billadenek kox"] = "Redroom";
+playerGuilds["po prostu wolnir"] = "Redroom";
+playerGuilds["młodyy al capone"] = "Redroom";
+playerGuilds["og farmer"] = "Redroom";
+playerGuilds["isirek"] = "Redroom";
+playerGuilds["żółw-"] = "Redroom";
+playerGuilds["scozzari"] = "Redroom";
+playerGuilds["knur opasy"] = "Redroom";
+playerGuilds["the shax"] = "Redroom";
+playerGuilds["slowpoke rodriguez"] = "Redroom";
+playerGuilds["gewehr"] = "Redroom";
+playerGuilds["odmów mi synu"] = "Redroom";
+playerGuilds["eovacius"] = "Redroom";
+playerGuilds["sokano"] = "Redroom";
+playerGuilds["dynowiak"] = "Redroom";
+playerGuilds["yankosek"] = "Redroom";
+playerGuilds["sypie się papier"] = "Redroom";
+playerGuilds["broshey"] = "Redroom";
+playerGuilds["frax"] = "Redroom";
+playerGuilds["razta"] = "Redroom";
+playerGuilds["hastosław"] = "Redroom";
+playerGuilds["turutek"] = "Redroom";
+playerGuilds["kacpervip"] = "Redroom";
+playerGuilds["brachol"] = "Redroom";
+playerGuilds["holiday pawulon"] = "Redroom";
+playerGuilds["andreus"] = "Redroom";
+playerGuilds["brr-brr-patapim"] = "Redroom";
+playerGuilds["frigo-camelo"] = "Redroom";
+playerGuilds["kolczar"] = "Redroom";
+playerGuilds["scigussek"] = "Redroom";
+playerGuilds["spalacz tkanki"] = "Redroom";
+playerGuilds["lupus terrorist"] = "Redroom";
+playerGuilds["czołg-"] = "Redroom";
+playerGuilds["keef papaj"] = "Redroom";
+playerGuilds["provokatiąun"] = "Redroom";
+playerGuilds["uber comfort"] = "Redroom";
+playerGuilds["rozrywkowy menix"] = "Redroom";
+playerGuilds["varrezinho"] = "Redroom";
+playerGuilds["kinio z pawlem"] = "Redroom";
+playerGuilds["psotnik łobuziak"] = "Redroom";
+playerGuilds["fisherprice"] = "Redroom";
+playerGuilds["bartek na teze"] = "Redroom";
+playerGuilds["tabluga"] = "Redroom";
+playerGuilds["sokano na teze"] = "Redroom";
+playerGuilds["san andreas"] = "Redroom";
+playerGuilds["hot ciżemka"] = "Redroom";
+playerGuilds["akwarelka"] = "Redroom";
